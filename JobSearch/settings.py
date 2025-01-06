5eb619345befd02f4297a99025a18ebddda51b09
Rohan8874 <shahmaouj517@gmail.com>
2025-01-06 16:18:35 +0600

    all changing add this push

--- JobSearch/settings.py
+++ JobSearch/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+
+    'company',
+    'dashboard',
+    'resume',
+    'app', 
+  
+
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +64,7 @@ ROOT_URLCONF = 'JobSearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +127,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = [
+    os.path.join(BASE_DIR / '/media')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app.User'
+AUTHENTICATION_BACKENDS = ['app.backends.EmailBacked']
--- app/views.py
+++ app/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .form import RegisterUserForm
+from resume.models import Resume
+from company.models import Company
 
-# Create your views here.
+def register_applicant(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_applicant =True 
+            var.username = var.email
+            var.save()
+            Resume.objects.create(user=var)
+            messages.info(request, 'Your account has been created.')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Something want wrong.')
+            return redirect('register_applicant')
+    else:
+        form = RegisterUserForm()
+        context = {'form':form}
+        return render(request, 'app/register_applicant.html', context)
+
+def register_recruiter(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_recruiter =True 
+            var.username = var.email
+            var.save()
+            Company.objects.create(user=var)
+            messages.info(request, 'Your account has been created.')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Something want wrong.')
+            return redirect('register_recruiter')
+    else:
+        form = RegisterUserForm()
+        context = {'form':form}
+        return render(request, 'app/register_recruiter.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Something went wrong')
+            return redirect('login')
+    else:
+        return redirect(request, 'app/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Your session has ended')
+    return redirect('login')
